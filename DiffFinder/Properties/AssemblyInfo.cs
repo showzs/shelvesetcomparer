9c13a29476da9a5f4c12d263bf41aab8aabcf370
dprZoft <73850562+dprZoft@users.noreply.github.com>
2022-07-08 21:05:07 +0200

    Update AssemblyInfo.cs
    Sync AssemblyInfo version with VSIX version

--- DiffFinder/Properties/AssemblyInfo.cs
+++ DiffFinder/Properties/AssemblyInfo.cs
@@ -23,5 +23,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("Release")]
 #endif
 
-[assembly: AssemblyVersion("1.0.4.19")]
-[assembly: AssemblyFileVersion("1.0.4.19")]
+[assembly: AssemblyVersion("1.4.1.19")]
+[assembly: AssemblyFileVersion("1.4.1.19")]
